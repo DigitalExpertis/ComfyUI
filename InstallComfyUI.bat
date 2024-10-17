818d301bdb7871a329f08e515dce2e118db34c72
Digital Expertis <andreas@digitalexpertis.se>
2024-10-18 01:37:43 +0200

    Ste 3

---
+++ InstallComfyUI.bat
@@ -0,0 +1,8 @@
+call python -m venv venv
+call venv\Scripts\activate
+call pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu124
+call pip install -r requirements.txt
+
+call main.py
+
+pause
---
+++ StartComfyUI.bat
@@ -0,0 +1,4 @@
+call venv\Scripts\activate
+
+call python main.py
+pause
