b476626d55d3a3eb7f4f2f188f05c380b5d088bd
Digital Expertis <andreas@digitalexpertis.se>
2024-10-18 10:23:35 +0200

    added more stuff

--- InstallComfyUI.bat
+++ InstallComfyUI.bat
@@ -7,6 +7,8 @@ call pip install -r custom_nodes\ComfyUI-Crystools\requirements.txt
 call pip install -r custom_nodes\comfyui_controlnet_aux\requirements.txt
 call pip install -r custom_nodes\ComfyUI-Florence2\requirements.txt
 call pip install -r custom_nodes\ComfyUI-KJNodes\requirements.txt
+call pip install -r custom_nodes\ComfyUI_essentials\requirements.txt
+
 call main.py
 
 pause
