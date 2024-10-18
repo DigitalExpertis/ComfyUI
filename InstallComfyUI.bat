31cf3c9fa72bf3c5ea0f47fb96bd1fdd0c5707e6
Digital Expertis <andreas@digitalexpertis.se>
2024-10-18 02:51:26 +0200

    Added controlnet

--- InstallComfyUI.bat
+++ InstallComfyUI.bat
@@ -3,6 +3,8 @@ call venv\Scripts\activate
 call pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu124
 call pip install -r requirements.txt
 call pip install -r custom_nodes\ComfyUI-Crystools\requirements.txt
+
+call pip install -r custom_nodes\comfyui_controlnet_aux\requirements.txt
 call main.py
 
 pause
